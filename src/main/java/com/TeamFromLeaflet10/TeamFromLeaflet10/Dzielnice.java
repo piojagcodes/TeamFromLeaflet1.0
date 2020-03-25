4e81beaef689b1f4c4ced93c3f9bedf76e922dfa
Piojag <piojag@gmail.com>
2020-03-25 11:31:42 +0100

    Pierwszy commit

--- src/main/java/com/TeamFromLeaflet10/TeamFromLeaflet10/Dzielnice.java
+++ src/main/java/com/TeamFromLeaflet10/TeamFromLeaflet10/Dzielnice.java
@@ -2,10 +2,15 @@ package com.TeamFromLeaflet10.TeamFromLeaflet10;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Dzielnice {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long ID;
     private String name;
     private float powierzchnia;
