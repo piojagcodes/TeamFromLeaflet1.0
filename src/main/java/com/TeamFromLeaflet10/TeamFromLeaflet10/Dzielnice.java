162c0b50a08f7ce7d27d27f6eddbcd5a29feb840
Piojag <piojag@gmail.com>
2020-03-25 11:26:41 +0100

    Pierwszy commit

--- src/main/java/com/TeamFromLeaflet10/TeamFromLeaflet10/Dzielnice.java
+++ src/main/java/com/TeamFromLeaflet10/TeamFromLeaflet10/Dzielnice.java
@@ -5,4 +5,9 @@ import javax.persistence.Entity;
 
 @Entity
 public class Dzielnice {
+
+    private Long ID;
+    private String name;
+    private float powierzchnia;
+    private int Liczba_Ludnosci;
 }
