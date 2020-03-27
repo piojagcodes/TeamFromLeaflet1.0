0e6c0df9c73640018b7f227b349d2d723ea8e5dc
Piojag <piojag@gmail.com>
2020-03-27 09:17:54 +0100

    Patch 1.0

---
+++ src/main/java/com/TeamFromLeaflet10/TeamFromLeaflet10/District.java
@@ -0,0 +1,54 @@
+package com.TeamFromLeaflet10.TeamFromLeaflet10;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class District {
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long Id;
+    private String name;
+    private float area;
+    private int population;
+
+    private District() {
+
+    }
+}
